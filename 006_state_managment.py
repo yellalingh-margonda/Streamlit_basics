a162b624d2b1560cfe7c87eae96f5e1399c10709
yellalingh Margonda <110678081+yellalingh-margonda@users.noreply.github.com>
2026-05-03 03:54:01 +0530

    Update 006_state_managment.py updated

--- 006_state_managment.py
+++ 006_state_managment.py
@@ -5,33 +5,6 @@ st.header("Section")
 st.text("Simple text")
 st.write({"a": 1})
 
-name = st.text_input("Enter name")
-age = st.slider("Age", 0, 100)
-clicked = st.button("Submit")
-
-if clicked:
-    st.write(name, age)
-
-# Core rule in Streamlit layout
-#
-# 👉 Everything goes in the main page by default
-# 👉 Only st.sidebar explicitly sends content to the sidebar
-
-# means:
-#
-# “Create two columns inside the MAIN area”
-#
-# NOT sidebar.
-c1, c2 = st.columns(2)
-
-with c1:
-    st.write("Left")
-
-with c2:
-    st.write("Right")
-st.sidebar.write("Sidebar content")
-
-
 # Across button clicks (NO refresh)
 #
 # 👉 This works because:
